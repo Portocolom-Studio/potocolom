e3bfcdb24062ee7b09e7b3b9d2e149e8180a76fe
leonfullxr <l.elliottfuller@gmail.com>
2026-07-25 18:19:13 +0200

    Serve a static studio favicon for the tab and sidebar mark.
    Browsers still request /favicon.ico; without a static asset that 404'd
    and the brand mark could fail to show. Ship ico+svg under static/.
    
    Signed-off-by: leonfullxr <l.elliottfuller@gmail.com>

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.ico" sizes="any" />
+	<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
 	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 	<meta name="theme-color" content="#070b14" />
 </svelte:head>
